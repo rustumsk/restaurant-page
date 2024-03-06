2ceedf4243e3b16bcfce04672f63431ccdec4ebe
Rustum <rustumarellano@gmail.com>
2024-03-06 21:32:51 +0800

    initial design done

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Home Page',
+            template: './src/index.html',
+            minify: false,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    }
+
 }
